f5caac45911df601c08aef38996648d068b29698
Roy Chela <roychela@gmail.com>
2019-02-08 00:00:33 +0300

    [Finishes #163712774] Created the POST office API endpoint

--- app/V1/routes/offices/__init__.py
+++ app/V1/routes/offices/__init__.py
@@ -8,3 +8,27 @@ def get_all_offices():
     return make_response(jsonify({
         "status": 200,
         "data": PoliticalOfficeModel.view_all_offices() }), 200)
+
+@bluprint.route("/offices", methods= ["POST"])
+def create_office():
+    data = request.get_json()
+    try:
+        name = data['name']
+        id = data['id']
+        type = data['type']
+    except:
+        return make_response(jsonify({
+            "status": 400,
+            "error": "All fields must be filled"
+        }), 400)
+    newoffice = PoliticalOfficeModel(name=name, type=type, id=id)
+    newoffice.saveoffice()
+
+    return make_response(jsonify({
+        "status": 201,
+        "data": [{
+            "id": id,
+            "name": name,
+            "type": type
+            }]
+    }), 201)        
