19a688dea9f1d36bfa68d1625e817b092ba95304
Roy Chela <roychela@gmail.com>
2019-02-09 14:45:41 +0300

    [Finishes #163842045] remove code init files

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from app.V1.views.offices import bluprint as officesblueprint
-from app.V1.views.parties import bluprint as partiesblueprint
+from app.V1.views.officeview import bluprint as officesblueprint
+from app.V1.views.partyview import bluprint as partiesblueprint
 
 def politicoapp():
     app = Flask(__name__)
