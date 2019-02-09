55029ff150fd7ee39e5c36f3f9a98de1f3c94672
Roy Chela <roychela@gmail.com>
2019-02-09 10:40:36 +0300

    [Finishes #163841965]

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from app.V1.routes.offices import bluprint as officesblueprint
-from app.V1.routes.parties import bluprint as partiesblueprint
+from app.V1.views.offices import bluprint as officesblueprint
+from app.V1.views.parties import bluprint as partiesblueprint
 
 def politicoapp():
     app = Flask(__name__)
